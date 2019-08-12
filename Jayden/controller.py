1e5077d8868c45bfd6991290c2489e14b482cd24
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2019-08-12 15:33:25 +0100

    Create controller.py

---
+++ Jayden/controller.py
@@ -0,0 +1,35 @@
+from microbit import *
+import radio
+
+radio.config(channel=11, group=210, queue=1)
+radio.on()
+'''
+INPUTS
+joystick:bit
+'''
+buttons = {2: 'A',
+           516: 'B',
+           684: 'C',
+           768: 'D',
+           853: 'E',
+           819: 'F'}
+
+def button_press():
+    press = pin2.read_analog()
+    if press < 900:
+        for number in range(press-5, press+5):
+            if number in buttons:
+                return buttons[number]
+
+def joystick_push():
+    x = pin0.read_analog()-517
+    y = pin1.read_analog()-515
+    left= y+x
+    right= y-x
+    return  left, right
+
+while True:
+    joystick = joystick_push()
+    message = str(joystick[0]) + " " + str(joystick[1])
+    radio.send(message)
+    sleep(10)
