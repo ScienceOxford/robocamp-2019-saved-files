7dce52bd2d2aff7e4bcf781012aa166695fbc1da
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2019-08-12 15:40:52 +0100

    Create controller.py

---
+++ Josie/controller.py
@@ -0,0 +1,43 @@
+from microbit import *
+import radio
+
+radio.config(channel=2, group=155, queue=1)
+radio.on()
+
+# Please tag us if used!
+# We'd love to see what you make:
+# @ScienceOxford
+
+'''
+INPUTS
+joystick:bit
+'''
+buttons = {2: 'A',
+           516: 'B',
+           684: 'C',
+           768: 'D',
+           853: 'E',
+           819: 'F'}
+
+def button_press():
+    press = pin2.read_analog()
+    if press < 900:
+        for number in range(press-5, press+5):
+            if number in buttons:
+                return buttons[number]
+
+def joystick_push():
+    x = pin0.read_analog() - 514
+    y = pin1.read_analog() - 516
+    left = y+x
+    right = y-x
+    return left, right
+
+while True:
+    if button_a.was_pressed():
+        radio.send('forward')
+
+    joystick = joystick_push()
+    message = str(joystick[0]) + " " + str(joystick[1])
+    radio.send(message)
+    sleep(10)
